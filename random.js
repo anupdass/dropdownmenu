6d2038674424eee0ee0a008fe69cc13a4a4d21c9
anupdass <anupdas633@gmail.com>
2021-05-06 17:46:39 +0600

    first commit

---
+++ random.js
@@ -0,0 +1,4 @@
+const months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
+const randomMonth = months[Math.floor(Math.random() * months.length)];
+
+console.log("random month =>", randomMonth);
